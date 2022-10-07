b7c10132609ebd1dfc13ee0e40f31f098e04a643
Marco Arnulfo <arnulfomarco7@gmail.com>
2022-10-07 17:50:09 +0200

    end project

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,10 +31,24 @@ buttonGenerator.addEventListener("click" , function(){
     const scontoOver = (price - price * discountOver)
 
     if (userAge.value < 18){
-        console.log(scontoMinorenne.toFixed(2) , "applicato sconto minorenne");
+        document.getElementById("nome_del_passeggero").innerHTML = nomeUtente.value
+        document.getElementById("tipo_sconto").innerHTML = "Sconto Minorenne"
+        document.getElementById("numero_carrozza").innerHTML = Math.floor((Math.random() * 10) + 1)
+        document.getElementById("codice_cp").innerHTML = Math.floor((Math.random() * 100000) + 1)
+        document.getElementById("costo_biglietto").innerHTML = scontoMinorenne.toFixed(2)
+
+        // console.log(scontoMinorenne.toFixed(2) , "applicato sconto minorenne");
+        // console.log(nomeUtente.value);
+        // console.log(userAge.value);
 
     } else if (userAge.value > 65){
-        console.log(scontoOver.toFixed(2) , "applicato sconto over65");
+        document.getElementById("nome_del_passeggero").innerHTML = nomeUtente.value
+        document.getElementById("tipo_sconto").innerHTML = "Sconto over 65"
+        document.getElementById("numero_carrozza").innerHTML = Math.floor((Math.random() * 10) + 1)
+        document.getElementById("codice_cp").innerHTML = Math.floor((Math.random() * 100000) + 1)
+        document.getElementById("costo_biglietto").innerHTML = scontoOver.toFixed(2)
+
+        // console.log(scontoOver.toFixed(2) , "applicato sconto over65");
     } 
 }) 
 
